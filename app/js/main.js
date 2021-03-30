48f8572ac2b10f90cba5b0a57aba6e308d6fe3b0
Alexander <kaverin125@gmail.com>
2021-03-30 22:53:36 +0300

    'adaptationHeader'

--- app/js/main.js
+++ app/js/main.js
@@ -1,49 +1,43 @@
-// const btnMenu = document.querySelector(".open-menu-btn"),
-//   $body = document.querySelector("body"),
-//   scrollLink = document.querySelectorAll(".scroll-link"),
-//   menuHeader = document.querySelector(".menu");
-//
-// const openMenuHeader = (e) => {
-//   e.preventDefault();
-//   const elem = e.target;
-//   console.log(elem);
-//   if (elem.closest(".open-menu-btn")) {
-//     menuHeader.classList.toggle("active-menu");
-//     document.documentElement.scrollIntoView({
-//       behavior: "smooth",
-//       block: "start",
-//     });
-//   }
-//   if (
-//     elem.classList.contains("menu") ||
-//     elem.classList.contains("menu__link")
-//   ) {
-//     menuHeader.classList.remove("active-menu");
-//   }
-// };
-// menuHeader.addEventListener("click", openMenuHeader);
+const btnMenu = document.querySelector(".open-menu-btn"),
+  	  $body = document.querySelector("body"),
+      scrollLink = document.querySelectorAll(".scroll-link"),
+	  menuHeader = document.querySelector(".header-menu");
+
+const openMenuHeader = (e) => {
+  e.preventDefault();
+  const elem = e.target;
+  console.log(elem)
+  if (elem.closest(".open-menu-btn")) {
+    menuHeader.classList.toggle("active");
+    document.documentElement.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  }
+  if (elem.classList.contains("header-menu") || elem.classList.contains("header-menu__link")) {
+    menuHeader.classList.remove("active-menu");
+  }
+};
+
+btnMenu.addEventListener("click", openMenuHeader);
+/*
 // // Плавная прокрутка к секции
-// for (let link of scrollLink) {
-//   link.addEventListener("click", function (e) {
-//     e.preventDefault();
-//     const blockID = link.getAttribute("href");
-//     $body.style.position = "static";
-//
-//     document.querySelector(blockID).scrollIntoView({
-//       behavior: "smooth",
-//       block: "start",
-//     });
-//   });
-// }
+for (let link of scrollLink) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const blockID = link.getAttribute("href");
+    $body.style.position = "static";
 
-// $(document).ready(function(){
-// 	$(".slider-users").owlCarousel({
-// 		items: 1,
-// 		stagePadding: 100,
-// 	});
-// });
+    document.querySelector(blockID).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  });
+}
+*/
 
 
+// Слайдер
 $(document).ready(function () {
 	$('.slider-users').slick({
 		centerMode: true,
