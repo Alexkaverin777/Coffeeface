5ef390d7d8db84a7a7021a1d18914d791729476b
Alexander <kaverin125@gmail.com>
2021-04-25 17:35:56 +0300

    'footer'

--- app/js/main.js
+++ app/js/main.js
@@ -1,40 +1,36 @@
-const btnMenu = document.querySelector(".open-menu-btn"),
-  	  $body = document.querySelector("body"),
-      scrollLink = document.querySelectorAll(".scroll-link"),
-	  menuHeader = document.querySelector(".header-menu");
+const btnMenuOpen = document.querySelector(".open-menu-btn"),
+	$body = document.querySelector("body"),
+	scrollLink = document.querySelectorAll(".scroll-link"),
+	menuHeader = document.querySelector(".header-menu"),
+	headerMenuActive = document.querySelector('.menu-header-wrapper');
 
+
+// Открытие меню
 const openMenuHeader = (e) => {
-  e.preventDefault();
-  const elem = e.target;
-  console.log(elem)
-  if (elem.closest(".open-menu-btn")) {
-    menuHeader.classList.toggle("active");
-    document.documentElement.scrollIntoView({
-      behavior: "smooth",
-      block: "start",
-    });
-  }
-  if (elem.classList.contains("header-menu") || elem.classList.contains("header-menu__link")) {
-    menuHeader.classList.remove("active-menu");
-  }
+	e.preventDefault();
+	const elem = e.target;
+	if (elem.closest(".open-menu-btn")) {
+		menuHeader.classList.toggle("menu-active");
+	}
+	if (elem.closest('.header-menu')) {
+		menuHeader.classList.remove("menu-active");
+	}
 };
+headerMenuActive.addEventListener("click", openMenuHeader);
 
-btnMenu.addEventListener("click", openMenuHeader);
-/*
-// // Плавная прокрутка к секции
+// Плавная прокрутка к секции
 for (let link of scrollLink) {
-  link.addEventListener("click", function (e) {
-    e.preventDefault();
-    const blockID = link.getAttribute("href");
-    $body.style.position = "static";
+	link.addEventListener("click", function (e) {
+		e.preventDefault();
+		const blockID = link.getAttribute("href");
+		$body.style.position = "static";
 
-    document.querySelector(blockID).scrollIntoView({
-      behavior: "smooth",
-      block: "start",
-    });
-  });
+		document.querySelector(blockID).scrollIntoView({
+			behavior: "smooth",
+			block: "start",
+		});
+	});
 }
-*/
 
 
 // Слайдер
@@ -53,7 +49,7 @@ $(document).ready(function () {
 					centerPadding: '18%',
 
 				}
-			},			{
+			}, {
 				breakpoint: 1200,
 				settings: {
 					centerMode: true,
@@ -71,11 +67,11 @@ $(document).ready(function () {
 				}
 			},
 			{
-				breakpoint: 480,
+				breakpoint: 576,
 				settings: {
 					arrows: false,
 					centerMode: true,
-					centerPadding: '10%',
+					centerPadding: false,
 					slidesToShow: 1
 				}
 			}
@@ -91,24 +87,59 @@ $(document).ready(function () {
 		nextArrow: '<button type="button" class="face-slider-next"></button>',
 		responsive: [
 			{
-				breakpoint: 768,
+				breakpoint: 992,
 				settings: {
 					arrows: false,
 					centerMode: true,
+					slidesToScroll: 1,
 					slidesToShow: 4
+				}
+			},
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 3
 				}
 			},
 			{
 				breakpoint: 480,
 				settings: {
-					arrows: false,
-					centerMode: true,
+					slidesToShow: 3
+				}
+			},
+			{
+				breakpoint: 380,
+				settings: {
 					slidesToShow: 2
 				}
 			}
 		]
 	});
 
+	$('.blog-slider').slick({
+		slidesToShow: 3,
+		touchMove: true,
+		slidesToScroll: 1,
+		arrows: false,
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					arrows: false,
+					infinite: false,
+					slidesToShow: 2.3
+				}
+			},
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 1.3,
+					infinite: false,
+
+				}
+			},
+		]
+	})
 
 
 });
